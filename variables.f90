3a061d61ddfefe8d40e25ec355a372abe37f7667
M.ngugi <mngugi7@gmail.com>
2023-09-03 10:04:49 +0300

    Update variables.f90

--- variables.f90
+++ variables.f90
@@ -1,20 +1,24 @@
-program variables
-    implicit none
+! This is a Fortran program that declares and prints various types of variables.
 
-    integer :: area      ! Declares an integer variable named "area"
-    real :: pi           ! Declares a real (floating-point) variable named "pi"
-    complex :: degrees   ! Declares a complex variable named "degrees"
-    character :: vowels  ! Declares a character variable named "vowels"
-    logical :: binaries  ! Declares a logical variable named "binaries"
+! Declare variables with their respective types
+implicit none
 
-    ! In Fortran, variables are not automatically initialized to zero or any other value.
+integer :: area      ! Declares an integer variable named "area"
+real :: pi           ! Declares a real (floating-point) variable named "pi"
+complex :: degrees   ! Declares a complex variable named "degrees"
+character :: vowels  ! Declares a character variable named "vowels"
+logical :: binaries  ! Declares a logical variable named "binaries"
 
-    ! Print the values of the declared variables
-    print *, 'The value of area (integer) is: ', area
-    print *, 'The value of pi (real) is: ', pi
-    print *, 'The value of degrees (complex) is: ', degrees
-    print *, 'The value of vowels (character) is: ', vowels
-    print *, 'The value of binaries (logical) is: ', binaries
+! In Fortran, variables are not automatically initialized to zero or any other value.
+! This means that the initial values of these variables are undefined.
+
+! Print the values of the declared variables
+print *, 'The value of area (integer) is: ', area
+print *, 'The value of pi (real) is: ', pi
+print *, 'The value of degrees (complex) is: ', degrees
+print *, 'The value of vowels (character) is: ', vowels
+print *, 'The value of binaries (logical) is: ', binaries
 
 end program variables
+
 
